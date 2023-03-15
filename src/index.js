11b2ac9596260d68498073fb18c482b09e27db2f
AndreaRoa <andrearoa182@gmail.com>
2023-03-15 18:14:08 -0500

    se realizo funcion para que las letras salgan en mayuscula

--- src/index.js
+++ src/index.js
@@ -1,35 +1,22 @@
-    document.getElementById("textOne").addEventListener("keyup",result); 
-        function result(e){
-       let palabra = e.target.value.toUpperCase();
-        this.value =palabra;
-       console.log(palabra)
+document.getElementById("textOne").addEventListener("keyup",result); 
+ function result(e){
+  let palabra = e.target.value.toUpperCase();
+  this.value =palabra;
+  let frase = document.getElementById("textOne").value;
+  let desplazamiento = document.getElementById("displacement").value;
+     console.log(desplazamiento);
+     }
+     
+     
 
-       document.getElementById("encode").addEventListener("click",cifrar);
-        function cifrar(e){
-        let frase = document.getElementById("textOne").value;
-        console.log(frase)
-        let desplazamiento = document.getElementById("displacement").value;
-        document.getElementById("messageOne").value = cifrar(frase,desplazamiento);
-     };
+         
 
 
-                };
-
+         
     
 
 
 
 
 
-     document.getElementById("cipher").addEventListener("click",descifrar);
-     function descifrar(e){
-         let frase = document.getElementById("textOne").value;
-         let desplazamiento = document.getElementById("displacement").value;
-         document.getElementById("messageTwo").value = descifrar(frase,desplazamiento);
-      };
-
-      function cifrar(frase,desplazamiento){
-        let resultado = "";
-        let abcdario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-      }
+   
