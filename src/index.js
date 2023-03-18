29a7c2ce64d2a443a381b76fc648626a3b51668d
Andrea Roa <andrearoa182@gmail.com>
2023-03-17 20:46:25 -0500

    se agrego funciones para cifrar y decifrar

--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
-document.getElementById("textOne").addEventListener("keyup",result); 
- function result(e){
-  let palabra = e.target.value.toUpperCase();
-  this.value =palabra;
-  let frase = document.getElementById("textOne").value;
-  let desplazamiento = document.getElementById("displacement").value;
-     console.log(desplazamiento);
-     }
-     
-     
+document.getElementById("encode").addEventListener("click",cipher);
+function cipher(){
+    let saveMessage = document.getElementById("message").value;
+    let newMessage = "";
+   let offset = Number(displacement.value);
+   for (let i = 0; i<saveMessage.length; i++){
+      let messageAscci = saveMessage.charCodeAt(i);
 
-         
+      if (messageAscci >= 65 && messageAscci <= 90){
+         newMessage += String.fromCharCode(((messageAscci - 65 + offset)  % 26)+ 65);
+         console.log(newMessage);
+           }
+      if(messageAscci >=97 && messageAscci <=122){
+         newMessage += String.fromCharCode(((messageAscci - 97 + offset)  % 26)+ 97);   
+         }
+      if (messageAscci === 32) {
+        newMessage += " ";
+        }
+         messageTwo.innerHTML = (newMessage);
+   }
+   }
 
+document.getElementById("decipher").addEventListener("click",decipher);
+function decipher(){
+   let saveMessage = document.getElementById("message").value;
+   let newMessage = "";  
+   let offset = Number(displacement.value);
+   for (let i = 0; i<saveMessage.length; i++){
+      let messageAscci = saveMessage.charCodeAt(i);
 
-         
-    
-
-
-
-
-
-   
+      if (messageAscci >= 65 && messageAscci <= 90){
+         newMessage += String.fromCharCode(((messageAscci - 65 - offset)  % 26)+ 65);
+         console.log(newMessage);
+           }
+      if(messageAscci >=97 && messageAscci <=122){
+         newMessage += String.fromCharCode(((messageAscci - 97 - offset)  % 26)+ 97);   
+         }
+      if (messageAscci === 32) {
+        newMessage += " ";
+        }
+         messageTwo.innerHTML = (newMessage);
+   }
+}
